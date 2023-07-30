3be3a759a37dc689632261d37c0e87916b30d8c4
Asadulla Hil Galib <140313892+galib1311@users.noreply.github.com>
2023-07-30 16:26:14 +0600

    Add files via upload

---
+++ LAB 01/mew4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>=0)
+    {
+        cout<<"x value is........"<<endl;
+        if(x%2==0)
+        {
+            cout<<"x value is........"<<endl;
+        }
+        if(x>999 & x<10000)
+        {
+            cout<<"x value contains........digits"<<endl;
+        }
+    }
+    return 0;
+}
